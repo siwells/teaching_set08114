9a8a5f1947cfa703a9f18f5a677af9bad9f5fb31
Simon Wells <siwells@gmail.com>
2016-02-11 18:33:51 +0000

    Add colour cycling button with logging example

---
+++ code/topic02/MultiColourButton+Logging/java/uk/ac/napier/multicolourbutton/MainActivity.java
@@ -0,0 +1,47 @@
+package uk.ac.napier.multicolourbutton;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    Boolean bg_blue = false;
+    Boolean bg_red = false;
+    Boolean bg_green = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        Button push_me = (Button) findViewById(R.id.btn_push_me);
+        push_me.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RelativeLayout rl1 = (RelativeLayout) findViewById(R.id.rl1);
+                if (bg_blue) {
+                    bg_blue = false;
+                    bg_red = true;
+                    bg_green = false;
+                    rl1.setBackgroundColor(Color.RED);
+                } else if (bg_red) {
+                    bg_blue = false;
+                    bg_red = false;
+                    bg_green = true;
+                    rl1.setBackgroundColor(Color.GREEN);
+                } else {
+                    bg_blue = true;
+                    bg_red = false;
+                    bg_green = false;
+                    rl1.setBackgroundColor(Color.BLUE);
+                }
+            }
+        });
+    }
+}
