e94ce87140922c79546f66a58b68f06a4cd9c92e
Simon Wells <siwells@gmail.com>
2016-02-18 15:24:47 +0000

    Add multi-activities example

---
+++ code/topic03/activities/java/uk/ac/napier/activitiesgalore/SecondActivity.java
@@ -0,0 +1,23 @@
+package uk.ac.napier.activitiesgalore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button btn = (Button) findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
